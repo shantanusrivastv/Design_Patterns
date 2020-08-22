736ecb859ebf65358e0fd035b43b5707a07bbcc1
Shantanu <shantanusrivastv-sw@yahoo.co.in>
2020-08-22 22:33:44 +0100

    Added Adabter pattern it changed the Weather service proj to use 4.6

---
+++ Adapter Pattern/Adabter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adapter_Pattern
+{
+
+        //  The payment interface
+        public interface IPayment
+        {
+
+            void takePayment(double Amount);
+        }
+
+        //  PayPal Payment class that implements the IPayment Interface
+        public class PayPalPayment : IPayment
+        {
+
+            public void takePayment(double Amount)
+            {
+                //  Code to take payment via PayPal merchant
+            }
+        }
+
+        //  Credit Card Payment class that implements the IPayment Interface
+        public class CreditCardPayment : IPayment
+        {
+
+            public void takePayment(double Amount)
+            {
+                //  Code to take payment via a credit card
+            }
+        }
+
+        //  Adapter Class to ensure third party code implements IPayment
+        public class ThirdPartyPaymentAdapter : ThirdPartyPayment, IPayment
+        {
+
+            public void takePayment(double Amount)
+            {
+                //  This methods calls the actual thrid party code.
+                //  If we couldn't inherit we could instantiate the object here or in the
+                //  constructor method.
+                base.makeTransasction(Amount, false);
+            }
+        }
+
+        //  Third Party Payment Class
+        public class ThirdPartyPayment
+        {
+
+            public void makeTransasction(double amount, bool refund)
+            {
+                //  Third party code...
+            }
+        }
+
+        //  Factory Class to return Concrete Payment object
+        public class PaymentFactory
+        {
+
+            public static IPayment getPayment(string PaymentType)
+            {
+                switch (PaymentType)
+                {
+                    case "CreditCard":
+                        return new CreditCardPayment();                       
+                    case "PayPal":
+                        return new PayPalPayment();                        
+                    default:
+                        return new ThirdPartyPaymentAdapter();                        
+                }
+            }
+        }
+    }
---
+++ Adapter Pattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adapter_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPayment aPayment;
+            string paymentType = "CreditCard";
+            //  this value would be populated elsewhere
+            aPayment = PaymentFactory.getPayment(paymentType);
+            aPayment.takePayment(10.5);
+
+            paymentType = "PayPal";
+            //  this value would be populated elsewhere
+            aPayment = PaymentFactory.getPayment(paymentType);
+            aPayment.takePayment(10.5);
+
+            paymentType = "3rd Party";
+            //  this value would be populated elsewhere
+            aPayment = PaymentFactory.getPayment(paymentType);
+            aPayment.takePayment(10.5);
+        }
+    }
+}
