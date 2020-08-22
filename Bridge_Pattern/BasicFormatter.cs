596acf1afa498ac432dda2f99ca321e765b54d6a
Shantanu <shantanusrivastv-sw@yahoo.co.in>
2020-08-22 23:22:06 +0100

    Implemented Bridge Pattern

---
+++ Bridge_Pattern/BasicFormatter.cs
@@ -0,0 +1,17 @@
+﻿using Bridge_Example;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge_Pattern
+{
+    public class BasicFormatter : IFormatter
+    {
+        public string Format(string key, string value)
+        {
+            return String.Format("{0}: {1}", key, value);
+        }
+    }
+}
--- Bridge_Pattern/Program.cs
+++ Bridge_Pattern/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Bridge_Pattern
+namespace Bridge_Example
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            List<Manuscript> documents = new List<Manuscript>();
+
+            //We can also use BasicFormatter, BackwardsFormatter
+            var formatter = new FancyFormatter();
+
+            var faq = new FAQ(formatter);
+            faq.Title = "The Bridge Pattern FAQ";
+            faq.Questions.Add("What is it?", "A design pattern");
+            faq.Questions.Add("When do we use it?", "When you need to separate an abstraction from an implementation.");
+            documents.Add(faq);
+
+            var book = new Book(formatter)
+                       {
+                           Title = "Lots of Patterns",
+                           Author = "John Sonmez",
+                           Text = "Blah blah blah..."
+                       };
+            documents.Add(book);
+
+            var paper = new TermPaper(formatter)
+                        {
+                            Class = "Design Patterns",
+                            Student = "Joe N00b",
+                            Text = "Blah blah blah...",
+                            References = "GOF"
+                        };
+            documents.Add(paper);
+
+            foreach(var doc in documents)
+            {
+                doc.Print();
+            }
+
+            Console.ReadKey();
         }
     }
 }
